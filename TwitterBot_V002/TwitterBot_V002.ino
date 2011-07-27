3bae2d95a353680b3b8da81c4b13215fbb1d1522
Chris Spurgeon <chris@spurgeonworld.com>
2011-07-26 23:17:14 -0700

    Starting over. Just using Arduino 22

--- TwitterBot_V002/TwitterBot_V002.ino
+++ TwitterBot_V002/TwitterBot_V002.ino
@@ -1,3 +1,8 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include <WProgram.h>
+#include <TextFinder.h>
+
 /*
   Web client
  
@@ -12,20 +17,23 @@
  
  */
 
-#include <SPI.h>
-#include <Ethernet.h>
+
 
 // Enter a MAC address for your controller below.
 // Newer Ethernet shields have a MAC address printed on a sticker on the shield
-byte mac[] = {  0x90, 0xA2, 0xDA, 0x00, 0x3A, 0x17 };
+byte mac[] = {  
+  0x90, 0xA2, 0xDA, 0x00, 0x3A, 0x17 };
 //IPAddress server(74,125,224,145); // Google
 IPAddress server(216,119,67,135); // spurgeonworld.com
+
+char twitterID[16];
 
 
 // Initialize the Ethernet client library
 // with the IP address and port of the server 
 // that you want to connect to (port 80 is default for HTTP):
 Client client;
+TextFinder finder(client);
 
 void setup() {
   // start the serial library:
@@ -34,8 +42,9 @@ void setup() {
   if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
     // no point in carrying on, so do nothing forevermore:
-    for(;;)
+    for(;;) {
       ;
+    }
   }
   // print your local IP address:
   Serial.print("My IP address: ");
@@ -47,41 +56,45 @@ void setup() {
   Serial.println();
 
   // give the Ethernet shield a second to initialize:
-  delay(1000);
+  delay(4000);
   Serial.println("connecting...");
+} // end of setup()
+
+
+
+void loop() {
 
   // if you get a connection, report back via serial:
   if (client.connect("www.spurgeonworld.com", 80)) {
     Serial.println("connected");
     // Make a HTTP request:
-//    client.println("GET /robots.txt HTTP/1.0");
+    //    client.println("GET /robots.txt HTTP/1.0");
     client.println("GET /twitterfeed/tweets.php?rpp=4&q=%40chrisspurgeon&since_id=95164107203952640 HTTP/1.0");
     client.println();
   } 
   else {
-    // kf you didn't get a connection to the server:
+    // if you didn't get a connection to the server:
     Serial.println("connection failed");
   }
-}
 
-void loop()
-{
-  // if there are incoming bytes available 
-  // from the server, read them and print them:
-  if (client.available()) {
-    char c = client.read();
-    Serial.print(c);
-  }
-
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
+  if (client.connected()) {
+    int theCount = finder.getString("\n","|",twitterID, 18);
+    Serial.print("the twitterID is ");
+    Serial.println(twitterID);
+  }  
+  else {
     Serial.println();
-    Serial.println("disconnecting.");
+    Serial.println("Not connected.");
+    client.flush();
     client.stop();
-
-    // do nothing forevermore:
-    for(;;)
-      ;
+    delay(10000);
   }
 }
+
+
+
+
+
+
+
 
